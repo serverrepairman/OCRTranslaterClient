4b977c4a818010df70324a29a20c41e92563dab9
serverrepairman <gustjwkd1007@gmail.com>
2021-12-19 17:48:37 +0900

    ㅍ1

--- app/src/main/java/org/techtown/ocrtranslaterclient/MainActivity.java
+++ app/src/main/java/org/techtown/ocrtranslaterclient/MainActivity.java
@@ -10,22 +10,27 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.audiofx.DynamicsProcessing;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -37,9 +42,10 @@ import org.techtown.ocrtranslaterclient.Configs;
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 0;
+    public SocketChannel socketChannel = null;
     private ImageView imageView;
     private TextView textView;
-    private Button button;
+    private Button button,button2;
     int PICK_IMAGE_MULTIPLE = 1;
     ArrayList<Drawable> images = new ArrayList<Drawable>();
     ArrayList<Uri> imagesUri = new ArrayList<Uri>();
@@ -53,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +79,19 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String data = encodeImage(imageView.getDrawable());
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        send(data);
+                    }
+                }).start();
             }
         });
     }
@@ -123,24 +143,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void send(String data){
-        try{
-            String host = Configs.host;
-            int portNumber = Configs.port;
-            Socket sock = new Socket(host, portNumber);
-            println("소켓 연결함.");
+        if(socketChannel == null || !socketChannel.isConnected()){
+            connectServer();
+        }
+        Thread sendingThread = new Thread() {
+            @Override
+            public void run() {
+                ByteBuffer byteBuffer = null; // 바이트 버퍼를 초기화한다.
+                Charset charset = Charset.forName("UTF-8");
+                byteBuffer = charset.encode(data);
+                try {
+                    socketChannel.write(byteBuffer);
+                } catch(Exception e) {
+//					label.setText(label.getText()+"\r\n"+"송신 실패...");
+                    println("송신 실패...");
+                    return;
+                }
+//				label.setText(label.getText()+"\r\n"+"송신:" + message);
+                println("송신 완료");
+                System.out.println("송신 : 길이 " + data.length());
+            }
+        };
+        sendingThread.start();
+    }
 
-            ObjectOutputStream outStream = new ObjectOutputStream(sock.getOutputStream());
-            outStream.writeObject(data);
-            outStream.flush();
-            println("데이터 전송함.");
-
-            ObjectInputStream inStream = new ObjectInputStream(sock.getInputStream());
-            println("서버로부터 받음 : "+ inStream.readObject());
-            sock.close();
-        }catch(Exception e){
-            e.printStackTrace();
+    public void connectServer(){
+        String ip = Configs.host;
+        int port = Configs.port;
+        try {
+            socketChannel = SocketChannel.open(); // 클라이언트 소켓 채널을 오픈한다.
+            socketChannel.configureBlocking(true); // 이 채널을 차단 모드로 둔다.
+            println("연결 요청..");
+        } catch(Exception e){
+            println("연결 요청 실패");
+        }
+        try {
+            socketChannel.connect(new InetSocketAddress(ip, port));
+            /* 클라이언트 소켓 채널이 서버에서 설정한 포트 번호와 호스트 네임으로 서버 소켓 채널과 연결을 시도한다. */
+            println("연결 성공!");
+        } catch (Exception e) {
+            println("연결 실패...");
         }
     }
 
-    private void println(String data){textView.append(data);}
+    public String encodeImage(Drawable img){
+        println("encode started");
+        BitmapDrawable bitimg = (BitmapDrawable) img;
+        Bitmap bitmap = bitimg.getBitmap();
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteStream);
+        byte[] byteArray = byteStream.toByteArray();
+        String baseString = Base64.encodeToString(byteArray,Base64.DEFAULT);
+        return baseString;
+    }
+
+    private void println(String data){textView.append(data+"\n");}
 }
